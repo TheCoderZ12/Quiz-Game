7f0dfdcdd9f011a6f7f1a4be4944dc179c41658c
TheCoderZ12 <144936161+TheCoderZ12@users.noreply.github.com>
2023-10-02 01:35:59 -0700

    Add files via upload
    A quiz game upload by kartik coder

---
+++ app.js
@@ -0,0 +1,34 @@
+//Asks for username
+let username = prompt("Enter you Username to play the game !")
+//Show that you can play
+let begin = alert("You can play now !")
+//Question
+let q1 = prompt("How is the father of Javascript")
+let q2 = prompt("Who invented Html !")
+let q3 = prompt("Is Javascript Hypertext Markup lang")
+let q4 = prompt("Do javascript have funcations")
+
+//Questions Answer and if else statements
+if (q1 == "Brendan Eich" || q1 == "brendan eich") {
+    alert("1) Correct")
+} else {
+    alert("1) Wrong")
+}
+
+if (q2 == "Tim Berners-Lee" || q2 == "tim berners-lee") {
+    alert("2) Correct")
+} else {
+    alert("2) Wrong")
+}
+
+if (q3 == "No" || q3 == "no") {
+    alert("3) Correct")
+} else {
+    alert("3) Wrong")
+}
+
+if (q4 == "Yes" || q4 == "yes") {
+    alert("4) Correct")
+} else {
+    alert("4) Wrong")
+}
